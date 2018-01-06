c634ed3769a1a43e5741e6c66bdfc4928bd2310b
Cedric Nicoloso <cnicoloso@octo.com>
2018-01-06 11:54:40 +1100

    A bit of styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
 // import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(
   <Docs />,
