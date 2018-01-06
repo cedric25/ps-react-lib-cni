65152bd6be5be5c4bbf98dcd54ede865905ec7e9
Cedric Nicoloso <cnicoloso@octo.com>
2018-01-06 13:12:52 +1100

    Include highlight.js style + drop some improvements as comments

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
 // import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+// https://highlightjs.org/static/demo/
+import '../node_modules/highlight.js/styles/atom-one-light.css'
 
 ReactDOM.render(
   <Docs />,
   document.getElementById('root'));
 
 // registerServiceWorker();
+
+// Improve:
+// - Search
+// - Might need to hide internal components (new folder 'internals')
+// - Group components (nested folders)
+// - Live props toggling
+// - Markdown support within descriptions
+// ( - Live editing ) (Ex: react-toolbox) or links to ready-to-use JSBin, Code Sandbox...
