2eb51304c7a537aa22d1689d79d47f326015d197
Cedric Nicoloso <cnicoloso@octo.com>
2018-01-13 12:27:41 +1100

    Comment CSS Modules option from webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -184,8 +184,8 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
-                        modules: true,
-                        localIdentName: '[name]_[local]_[hash:base64:5]'
+                        // modules: true,
+                        // localIdentName: '[name]_[local]_[hash:base64:5]'
                       },
                     },
                     {
